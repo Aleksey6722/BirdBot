1aeece6fbf518028186147633cbb9b026c7267ca
Aleksey <Aleksey6722@mail.ru>
2023-07-19 21:55:01 +0600

    database filling from new users

--- birds_bot_start.py
+++ birds_bot_start.py
@@ -1,19 +1,46 @@
 import telebot
 import os
 import csv
+from sqlalchemy import exc
 
+from models import session, Bird, User, UserBird
 TOKEN = os.getenv('BIRDS_BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 
-def checking_csv(path):
+def checking_csv(csv_file):
     example = ['Row #', 'Taxon Order', 'Category', 'Common Name', 'Scientific Name', 'Count', 'Location', 'S/P',
                'Date', 'LocID', 'SubID', 'Exotic', 'Countable']
-    with open(path, 'r') as csv_file:
+    with open(csv_file, 'r') as csv_file:
         reader = csv.reader(csv_file)
         first_row = next(reader)
-        return False if first_row != example else True
+        return first_row == example
 
+
+def database_filling(message, csv_file):
+    user = session.query(User).filter(User.chat_id == message.chat.id).first()
+    if not user:
+        new_user = User(name=message.chat.first_name, chat_id=message.chat.id)
+        session.add(new_user)
+        session.commit()
+        a = new_user.id
+        with open(csv_file, 'r', encoding='UTF-8') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                common_name = row.get('Common Name')
+                scientific_name = row.get('Scientific Name')
+                new_bird = Bird(common_name=common_name, scientific_name=scientific_name)
+                session.add(new_bird)
+                try:
+                    session.commit()
+                    id = new_bird.id
+                except exc.IntegrityError:
+                    session.rollback()
+                    id = session.query(Bird).filter(Bird.scientific_name == scientific_name).first().id
+                userbird = UserBird(user_id=new_user.id, bird_id=id)
+                session.add(userbird)
+                session.commit()
+            bot.send_message(message.chat.id, 'Список птиц создан')
 
 
 @bot.message_handler(commands=['start'])
@@ -29,13 +56,13 @@ def get_csv(message):
     file_name = message.document.file_name
     file_info = bot.get_file(message.document.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-    path = os.path.join(os.curdir, 'temp', file_name)
-    with open(path, 'wb') as file:
+    csv_file = os.path.join(os.curdir, 'temp', file_name)
+    with open(csv_file, 'wb') as file:
         file.write(downloaded_file)
-    if checking_csv(path):
-        bot.send_message(message.chat.id, 'Файл принят!')
-    else:
+    if not checking_csv(csv_file):
         bot.send_message(message.chat.id, 'Файл не соответствует образцу')
+        return
+    database_filling(message, csv_file)
 
 
 
---
+++ models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine, String, Integer, Column, ForeignKey
+from sqlalchemy.orm import Session, declarative_base, relationship
+import os
+
+USER = os.getenv('USER_DB')
+PASSWORD = os.getenv('PASSWORD_DB')
+engine = create_engine(f"postgresql+psycopg2://{USER}:{PASSWORD}@localhost/birdbot")
+session = Session(bind=engine)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    chat_id = Column(Integer, nullable=False)
+    birds = relationship('UserBird', backref='user')
+
+
+class Bird(Base):
+    __tablename__ = 'bird'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    common_name = Column(String(100), nullable=False, unique=True)
+    scientific_name = Column(String(100), nullable=False, unique=True)
+    users = relationship('UserBird', backref='bird')
+
+
+class UserBird(Base):
+    __tablename__ = 'userbird'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer(), ForeignKey('user.id'))
+    bird_id = Column(Integer(), ForeignKey('bird.id'))
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
