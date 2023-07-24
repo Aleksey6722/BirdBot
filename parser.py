67c56cc280cf35bc74c7aa7cfade9f59e0cf7002
Aleksey <Aleksey6722@mail.ru>
2023-07-24 22:27:08 +0600

    parsing main page

--- parser.py
+++ parser.py
@@ -1,11 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://kz.birds.watch/"
-main_page = requests.get(url)
+import re
+from datetime import datetime
 
-soup = BeautifulSoup(main_page.text, 'lxml')
-# box = soup.find('b', string="what's new?")
-box = soup.find('section', class_='orta').find('a', recursive=False).previous_sibling
-print(box)
+main_url = "https://kz.birds.watch/"
+main_page = requests.get(main_url)
+
+soup = BeautifulSoup(main_page.content, 'lxml')
+news_div = soup.find('section', class_='orta').find('a', recursive=False).find_next_sibling()
+the_latest_p = news_div.find('p')
+date_tag = the_latest_p.find(string=re.compile('^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01]).$'))
+today = datetime.today().strftime('%Y-%m-%d')
+date_from_page = date_tag.text.strip('.')
+link_tags = the_latest_p.find_all('a', href=True)
+for tag in link_tags:
+    link = tag['href']
+    bird_url = main_url+link
+    bird_page = requests.get(bird_url)
+
 pass
+
